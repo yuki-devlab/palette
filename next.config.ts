2fac3d31115ced8b8dd1d2262309420b66fe35a0
yuki-devlab <yuki61051108@gmail.com>
2026-03-17 01:00:48 +0900

    chore: add images config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     devIndicators: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                port: "",
+                pathname: "",
+                search: "",
+            },
+        ],
+    },
 }
 
 export default nextConfig;
