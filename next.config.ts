8477b9321118fb1b96c1a1949a85fcf704bfe627
yuki-devlab <yuki61051108@gmail.com>
2026-03-18 01:10:46 +0900

    fix: remove port pathname search

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
             {
                 protocol: "https",
                 hostname: "lh3.googleusercontent.com",
-                port: "",
-                pathname: "",
-                search: "",
             },
         ],
     },
