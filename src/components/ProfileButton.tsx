5db93a0f57758520612765cc78bac6da274abdc5
yuki-devlab <yuki61051108@gmail.com>
2026-03-25 21:14:32 +0900

    feat: add ProfileButton.tsx

---
+++ src/components/ProfileButton.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import ProfileMenu from "@/components/ProfileMenu";
+
+export default function ProfileButton({ imgSrc }: Readonly<{ imgSrc: string }>) {
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+    const wrapperRef = useRef<HTMLDivElement>(null);
+
+    const closeMenu = () => {
+        setMenuIsOpen(false);
+    }
+
+    const toggleMenu = () => {
+        setMenuIsOpen((prev) => !prev);
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (
+                wrapperRef.current &&
+                !wrapperRef.current.contains(e.target as Node)
+            ) {
+                closeMenu();
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+    }, [])
+
+    return (
+        <div ref={wrapperRef} className="relative">
+            <button type="button" className="block" onClick={toggleMenu}>
+                <Image
+                    src={imgSrc}
+                    alt="プロフィール画像"
+                    width={40}
+                    height={40}
+                    className="border border-gray-200 rounded-full"
+                />
+            </button>
+            <ProfileMenu isOpen={menuIsOpen} />
+        </div>
+    )
+}
