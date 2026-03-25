5dd8621f1794268b66b61c6eb3c8eab4b6d49559
yuki-devlab <yuki61051108@gmail.com>
2026-03-26 03:24:56 +0900

    feat: add ProfileButton.tsx to Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,43 @@
-"use client";
-
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
-import Logo from "@/components/Logo";
+import { auth } from "@/lib/auth";
+import { Help } from "@material-symbols-svg/react";
+import prisma from "@/lib/prisma";
 import Container from "@/components/Container";
-import LogInButton from "@/components/LogInButton";
+import Logo from "@/components/Logo";
+import LoginButton from "@/components/LoginButton";
+import ProfileButton from "@/components/ProfileButton";
 
-export default function Header() {
-    const pathname = usePathname();
-    const isHome = pathname === "/";
+export default async function Header() {
+    const session = await auth();
+
+    let user = null;
+    if (session) {
+        user = await prisma.user.findUnique({
+            where: {
+                id: session.user?.id,
+            },
+        })
+    }
 
     return (
-        <header className="bg-white py-4">
-            <Container>
+        <header className="bg-white flex h-[72px] items-center">
+            <Container size="xl">
                 <div className="flex items-center justify-between">
                     <Link href="/">
-                        {isHome ? <h1><Logo /></h1> : <Logo />}
+                        <Logo />
                     </Link>
                     <div className="flex gap-6 items-center text-sm">
                         <Link href="/how-to-use" className="flex gap-1 items-center text-gray-500 hover:text-gray-700">
-                            <QuestionMarkCircleIcon className="size-5" />
+                            <Help size={20} />
                             <span className="[text-box:trim-both_cap_alphabetic]">
                                 使い方
                             </span>
                         </Link>
-                        <LogInButton />
+                        {!session ? (
+                            <LoginButton />
+                        ) : (
+                            <ProfileButton imgSrc={session.user?.image} />
+                        )}
                     </div>
                 </div>
             </Container>
