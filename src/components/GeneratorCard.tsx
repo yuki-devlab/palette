71c20e68c007a233722f64a55908b323a1558868
yuki-devlab <yuki61051108@gmail.com>
2026-03-27 03:57:31 +0900

    feat: add GeneratorCard.tsx

---
+++ src/components/GeneratorCard.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+interface GeneratorCardProps {
+    url: string;
+    label: string;
+    description: string;
+    GeneratorIcon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+}
+
+export default function GeneratorCard({ url, label, description, GeneratorIcon }: Readonly<GeneratorCardProps>) {
+    return (
+        <Link href={url} className="bg-white duration-300 flex flex-col rounded-3xl shadow-md transition-all hover:-translate-y-1 hover:shadow-lg">
+            <div className="bg-blue-200 flex h-[200px] items-center justify-center rounded-t-3xl">
+                <GeneratorIcon />
+            </div>
+            <div className="[text-box:trim-both_cap_alphabetic] px-6 py-7 space-y-6">
+                <h2 className="font-bold text-center text-gray-600">
+                    {label}
+                </h2>
+                <p className="leading-relaxed text-gray-500 text-xs">
+                    {description}
+                </p>
+            </div>
+        </Link>
+    )
+}
